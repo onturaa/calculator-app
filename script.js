c7947a6ed66f07c9f8cec23f27039ae3dcbb82d5
onturaa <174931989+onturaa@users.noreply.github.com>
2025-01-14 14:39:20 -0500

    Add Final Styling, Script to Calculator, Edit html
    Remove some layout divs in html file to make buttons
    into gird format, standardize button size, finish
    logic for calculator so everything attaches to
    correct variable and shows up properly in display.

--- script.js
+++ script.js
@@ -1,33 +1,95 @@
 let number1 = '';
 let operator = '';
 let number2 = '';
+const MAX_LENGTH = 20;
 
 
 function add(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return parseInt(a) - parseInt(b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return parseInt(a) * parseInt(b);
 }
 
 function divide(a, b) {
-    return a / b;
+    return parseInt(a) / parseInt(b);
 }
 
 function operate(operator, number1, number2) {
     switch (operator) {
-        case 'add':
+        case '+':
             return add(number1, number2);
-        case 'subtract':
+        case '-':
             return subtract(number1, number2);
-        case 'multiply':
+        case '*':
             return multiply(number1, number2);
-        case 'divide':
+        case '/':
             return divide(number1, number2);
     }
 }
+
+const display = document.querySelector(".display");
+
+const numBtns = document.querySelectorAll('.number-btn');
+
+numBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        if (operator === '') {
+            if (number1.length < MAX_LENGTH) {
+                display.textContent += button.textContent;
+                number1 += button.textContent;
+            }
+        }
+        else {
+            if (display.textContent === operator) {
+                display.textContent = button.textContent;
+                number2 += button.textContent;
+            } else {
+                if (number2.length < MAX_LENGTH) {
+                    display.textContent += button.textContent;
+                    number2 += button.textContent;
+                }
+            }
+        }
+    });
+});
+
+const operateBtns = document.querySelectorAll(".operator-btn");
+
+operateBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        if (number2 === '') {
+            if (number1 === '') {
+                number1 = '0';
+            }
+            display.textContent = button.textContent;
+            display.style.textAlign = "right";
+            operator = button.textContent;
+        }
+    });
+});
+
+const clear = document.querySelector("#clear-btn");
+
+clear.addEventListener('click', () => {
+    display.textContent = "";
+    number1 = '';
+    number2 = '';
+    operator = '';
+});
+
+const enter = document.querySelector('#enter-btn');
+
+enter.addEventListener('click', () => {
+    if (number1 !== '' && operator !== '' && number2 !== '') {
+        display.textContent = operate(operator, number1, number2);
+        number1 = display.textContent;
+        operator = '';
+        number2 = '';
+    }
+});
